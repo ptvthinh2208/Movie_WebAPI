595a6467826198d25936e0f497b14369715a5899
ptvthinh2208 <126745003+ptvthinh2208@users.noreply.github.com>
2023-03-03 17:02:45 +0700

    Create models : Actor, Country, Director, Genre, Movie

---
+++ Movie Services/MovieService.Domain/Country/Country.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MovieService.Domain.Country
+{
+    public class Country
+    {
+        public Country()
+        {
+            Movie = new HashSet<Movie.Movie>();
+        }
+        public int CountryId { get; set; }
+        public string CountryName { get; set; }
+        public ICollection<Movie.Movie> Movie { get; set; }
+    }
+}
---
+++ Movie Services/MovieService.Domain/Genre/Genre.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MovieService.Domain.Genre
+{
+    public class Genre
+    {
+        public Genre()
+        {
+            Movie = new HashSet<Movie.Movie>();
+        }
+        public int GenreId { get; set; }
+        public string GenreName { get; set; }
+        public ICollection<Movie.Movie> Movie { get; set; }
+    }
+}
