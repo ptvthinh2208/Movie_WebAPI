72eed2fc4c96ac6ee2fd0d82e5841d4bae7c2be1
ptvthinh2208 <126745003+ptvthinh2208@users.noreply.github.com>
2023-03-04 11:04:42 +0700

    (1).Create Interface IPropertyRepository represents entity Movie (2).Create class MovieFilterModel to show data Movie

---
+++ Movie Services/MovieService.Domain/Movie/IMovieRepository.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MovieService.Domain.Movie
+{
+    public interface IMovieRepository
+    {
+        Task<IEnumerable<Movie>> GetMovies(MovieFilterModel movieFilterModel);
+        Task<Movie> GetMoviesByNameVersion(MovieFilterModel movieFilterModel);
+        Task<IEnumerable<Movie>> GetMoviesByfilterModels(
+            IEnumerable<MovieFilterModel> movieFilterModels);
+        Task<Movie> GetMovieById(int movieId);
+        Task InsertMovie(Movie movie);
+        void UpdateMovie(Movie movie);
+        Task BulkUpdateMovie(IEnumerable<Movie> movies);
+    }
+}
---
+++ Movie Services/MovieService.Domain/Movie/MovieFilterModel.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MovieService.Domain.Movie
+{
+    public class MovieFilterModel
+    {
+        public string Actor { get; set; }
+        public string Country { get; set; }
+        public string Director { get; set; }
+        public string Genre { get; set; }
+        private int alwaysShow { get; set; }
+        public int AlwaysShow   // property
+        {
+            get { return alwaysShow; }   // get method
+            set { alwaysShow = value; }  // set method
+        }
+    }
+}
