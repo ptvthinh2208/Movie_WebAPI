7abd1efb2c0ca61100337b011e24bcf24d24f8a0
ptvthinh2208 <126745003+ptvthinh2208@users.noreply.github.com>
2023-03-04 10:56:23 +0700

    Create Interface IPropertyRepository represents a data entity (a table on the database), including common data access logic (CRUD) and other special logic

---
+++ Movie Services/MovieService.Domain/IPropertyRepository.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MovieService.Domain
+{
+    public interface IPropertyRepository<T> where T : class
+    {
+        Task<T> GetByName(string name);
+        Task<IEnumerable<T>> GetByNames(string[] names);
+        Task<IEnumerable<T>> Get();
+        Task<T> Get(string id);
+        Task<T> Get(int id);
+        Task Create(T entity);
+        void Update(T entity);
+        void Delete(T entity);
+    }
+}
