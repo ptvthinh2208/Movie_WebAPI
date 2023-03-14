b70164514eff8deebe194742a7fa814efc930191
ptvthinh2208 <126745003+ptvthinh2208@users.noreply.github.com>
2023-03-14 11:33:11 +0700

    Create GenreDto and GenreService

---
+++ MovieService/MovieService.Business/BusinessUseCases/PropertyGenreService.cs
@@ -0,0 +1,44 @@
+﻿using MovieService.Business.DTOs;
+using MovieService.Domain.Genre;
+using MovieService.Domain;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MovieService.Business.BusinessUseCases
+{
+    public class PropertyGenreService : BasePropertyService<Genre, GenreDto>
+    {
+
+        public PropertyGenreService(IUnitOfWork unitOfWork) : base(unitOfWork)
+        {
+
+        }
+        public override IPropertyRepository<Genre> GetPropertyRepository(IUnitOfWork unitOfWork)
+        {
+            return unitOfWork.GenreRepository;
+        }
+        public override Genre CreateEntity(string value)
+        {
+            return new Genre()
+            {
+                GenreName = value
+            };
+        }
+        public override Genre UpdateEntity(Genre entity, string value)
+        {
+            entity.GenreName = value;
+            return entity;
+        }
+        public override GenreDto MapToDto(Genre entity)
+        {
+            return new GenreDto()
+            {
+                GenreId = entity.GenreId,
+                GenreName = entity.GenreName
+            };
+        }
+    }
+}
