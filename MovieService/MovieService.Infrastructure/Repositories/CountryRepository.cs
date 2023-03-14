d7acb2aa8aea24b433540fa6f59ddd08cc094118
ptvthinh2208 <126745003+ptvthinh2208@users.noreply.github.com>
2023-03-14 11:06:31 +0700

    Create CountryRepository

---
+++ MovieService/MovieService.Infrastructure/Repositories/CountryRepository.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MovieService.Domain.Country;
+using MovieService.Infrastructure.EF;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MovieService.Infrastructure.Repositories
+{
+    public class CountryRepository : BasePropertyRepository<Country>
+    {
+        private readonly MovieContext _movieContext;
+        public CountryRepository(MovieContext movieContext) : base(movieContext)
+        {
+            _movieContext = movieContext;
+        }
+        public override async Task<Country> GetByName(string name)
+        {
+            return await _movieContext.Country.FirstOrDefaultAsync(x => x.CountryName == name);
+        }
+
+        public virtual async Task<IEnumerable<Country>> GetByNames(string[] names)
+        {
+            return await _movieContext.Country.Where(a => names.Any(x => x == a.CountryName)).ToArrayAsync();
+        }
+    }
+}
