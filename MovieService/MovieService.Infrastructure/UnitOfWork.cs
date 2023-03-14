a8a78f10f626a796912671a5ebda22651925dfd6
ptvthinh2208 <126745003+ptvthinh2208@users.noreply.github.com>
2023-03-14 11:32:15 +0700

    Inject GenreRepository to IUnitOfWork

--- MovieService/MovieService.Infrastructure/UnitOfWork.cs
+++ MovieService/MovieService.Infrastructure/UnitOfWork.cs
@@ -10,6 +10,7 @@ using System.Threading.Tasks;
 using MovieService.Infrastructure.Repositories;
 using MovieService.Domain.Country;
 using MovieService.Domain.Director;
+using MovieService.Domain.Genre;
 
 namespace MovieService.Infrastructure
 {
@@ -23,11 +24,14 @@ namespace MovieService.Infrastructure
         private IPropertyRepository<Actor> _actorRepository;
         private IPropertyRepository<Country> _countryRepository;
         private IPropertyRepository<Director> _directorRepository;
+        private IPropertyRepository<Genre> _genreRepository;
 
 
         public IPropertyRepository<Actor> ActorRepository => _actorRepository = new ActorRepository(_movieContext);
         public IPropertyRepository<Country> CountryRepository => _countryRepository = new CountryRepository(_movieContext);
         public IPropertyRepository<Director> DirectorRepository => _directorRepository = new DirectorRepository(_movieContext);
+        public IPropertyRepository<Genre> GenreRepository => _genreRepository = new GenreRepository(_movieContext);
+
         public void Save()
         {
             _movieContext.SaveChanges();
