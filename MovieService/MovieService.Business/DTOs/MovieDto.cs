946749ba37704cc503ab62f845781eed90b28502
ptvthinh2208 <126745003+ptvthinh2208@users.noreply.github.com>
2023-03-14 11:41:45 +0700

    Create MovieDto, MovieResponeDto and MovieMapper inside Mapper folder to map object

---
+++ MovieService/MovieService.Business/DTOs/MovieDto.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MovieService.Business.DTOs
+{
+    public class MovieDto
+    {
+        public int MovieId { get; set; }
+        public string Title { get; set; }
+        public int? Date { get; set; }
+        public int? Time { get; set; }
+        public string MovieName { get; set; }
+        public string ActorName { get; set; }
+        public string DirectorName { get; set; }
+        public string CountryName { get; set; }
+        public string GenreName { get; set; }
+
+    }
+}
---
+++ MovieService/MovieService.Business/DTOs/MovieResponeDto.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MovieService.Business.DTOs
+{
+    public class MovieResponeDto
+    {
+        public int MovieId { get; set; }
+        public string Title { get; set; }
+        public string MovieName { get; set; }
+    }
+}
