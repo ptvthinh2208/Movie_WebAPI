6e81a5debbdb73ec4241cc0da8ee4e2dbf06b571
ptvthinh2208 <126745003+ptvthinh2208@users.noreply.github.com>
2023-03-14 11:34:57 +0700

    Create GenreController and register the service <Genre> with scoped lifetime

---
+++ MovieService/MovieService/Controllers/PropertyGenreController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MovieService.Business.BusinessUseCases;
+using MovieService.Business.DTOs;
+using MovieService.Domain.Genre;
+
+namespace MovieService.Controllers
+{
+    [Route("api/genre")]
+    public class PropertyGenreController : BasePropertyController<Genre, GenreDto>
+    {
+        public PropertyGenreController(IPropertyService<Genre, GenreDto> propertyService)
+        {
+            SetService(propertyService);
+        }
+    }
+}
