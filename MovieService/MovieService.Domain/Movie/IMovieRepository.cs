b3430f803000db048d523faf00bc27462d324398
ptvthinh2208 <126745003+ptvthinh2208@users.noreply.github.com>
2023-03-22 18:22:37 +0700

    Create Interface IMovieRepository

---
+++ MovieService/MovieService.Domain/Movie/IMovieRepository.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MovieService.Domain.Movie
+{
+    public interface IMovieRepository
+    {
+        Task<Movie> GetByName(string name);
+        Task<IEnumerable<Movie>> GetByNames(string[] names);
+        Task<IEnumerable<Movie>> Get();
+        Task<Movie> Get(string id);
+        Task<Movie> Get(int id);
+        Task Create(Movie entity);
+        void Update(Movie entity);
+        void Delete(Movie entity);
+    }
+}
